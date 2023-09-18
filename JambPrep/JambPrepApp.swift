e5939ae5c9f30f34b79b2cc7af369dc37216e23a
Omnivrr <Omnivrr@gmail.com>
2023-09-18 23:35:37 +0100

    Initial Commit

---
+++ JambPrep/JambPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  JambPrepApp.swift
+//  JambPrep
+//
+//  Created by Bukhari Sani on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct JambPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
