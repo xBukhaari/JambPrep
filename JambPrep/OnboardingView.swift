771f489dc38e73996801c5353eff54430bb7cd10
Omnivrr <Omnivrr@gmail.com>
2023-09-19 01:22:06 +0100

    Jamb Prep

--- JambPrep/OnboardingView.swift
+++ JambPrep/OnboardingView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Feature: Identifiable {
     let id = UUID()
     let title: String
-    let desccription: String
+    let description: String
     let icon: String?
 }
 
@@ -37,7 +37,7 @@ struct OnboardingView: View {
             }
             .sheet(isPresented: $showingOnboarding) {
                 VStack {
-                    Text("Welcome to \(appName)")
+                    Text("Welcome to\(appName)")
                         .font(.largeTitle)
                         .fontWeight(.bold)
                         .padding(.vertical, 50)
@@ -56,19 +56,65 @@ struct OnboardingView: View {
                                             .foregroundColor(color ?? Color.green)
                                             .padding(.trailing, 15)
                                             .padding(.vertical, 15)
+                                        
                                     }
+                                    VStack(alignment: .leading) {
+                                        Text(feature.title)
+                                            .fontWeight(.bold)
+                                            .font(.system(size: 16))
+                                        Text(feature.description)
+                                            .font(.system(size: 15))
+                                        
+                                    }
+                                    Spacer()
                                 }
                             }
+                            .padding(.horizontal,20)
+                            .padding(.bottom, 20)
                         }
                     }
+                    .padding(.bottom, 30)
+                    Spacer()
+                    VStack {
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(color ?? Color.green)
+                                .cornerRadius(12)
+                                .frame(height: 65)
+                            Text("Continue")
+                                .fontWeight(.bold)
+                                .foregroundColor(.white)
+                        }
+                        .onDisappear() {
+                            UserDefaults.standard.set(true, forKey: "OnboardingSeen")
+                            closingOnboarding = true
+                        }
+                        .onTapGesture {
+                            showingOnboarding = false
+                            
+                        }
+                    }
+                    
+                    .padding(.top, 15)
+                    .padding(.bottom, 50)
+                    .padding(.horizontal, 15)
                 }
+                .padding()
             }
-        
     }
 }
 
 struct OnboardingView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        OnboardingView(appName: "JambPrep",
+                       features: [
+                        Feature(title: "Access To Past Question", description: "Over 100,000 past questions available from 1990-2022.", icon: "book.fill"),
+                        Feature(title: "Customize Your Subjects", description: "You can choose any combination of four subjects that align with your JAMB goals.", icon: "pencil"),
+                        Feature(title: "Comprehensive Result", description: "Receive detailed performance feedback upon completing practice tests.", icon: "chart.bar.xaxis"),
+                        Feature(title: "Extensive Subject Coverage", description: "Practice all the 25 subjects of JAMB", icon: "books.vertical.fill"),
+                        Feature(title: "Ad-Free Experience", description: "Thank you for downloading my app, practice and get that 300+ score champ!", icon: "party.popper.fill"),
+                  
+                       ],
+                       color: Color.green)
     }
 }
