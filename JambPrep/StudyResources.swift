45ef58a26acf06ef6df94c051d0d05022efb3e87
Omnivrr <Omnivrr@gmail.com>
2023-09-30 23:45:19 +0100

    SR

--- JambPrep/StudyResources.swift
+++ JambPrep/StudyResources.swift
@@ -8,42 +8,47 @@
 import SwiftUI
 
 struct StudyResources: View {
-    let gridItems = [
-         GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16),
-         GridItem(.flexible(minimum: 100, maximum: 200), spacing: 16),
-     ]
-     
-    var body: some View {
-        NavigationView {
-            ScrollView {
-                LazyVGrid(columns: gridItems, spacing: 16) {
-                    ForEach(items, id: \.self) { item in
-                        NavigationLink(destination: Text("Details for \(item)")) {
-                            ZStack(alignment: .bottom) {
-                                Image(systemName: "book.circle.fill")
-                                    .resizable()
-                                    .frame(width:150, height: 150)
-                                    .foregroundColor(Color.green)
-                                
-                                Text(item)
-                                    .foregroundColor(Color.white)
-                                    .font(.caption)
-                                    .padding(.bottom, 4)
+        let gridStudyItems = [
+            GridItem(.flexible(minimum: 150, maximum: 200), spacing: 15),
+            GridItem(.flexible(minimum: 150, maximum: 200), spacing: 15),
+        ]
+        
+        let itemImages = ["Textbooks", "Novel", "Syllabus", "Universities", "Polytechnics", "PostUTME", "DirectEntry", "ImportantDates", "Tips"]
+        
+        var body: some View {
+            NavigationView {
+                ScrollView {
+                    LazyVGrid(columns: gridStudyItems, spacing: 15) {
+                        ForEach(0..<items.count, id: \.self) { index in
+                            let item = items[index]
+                            let imageName = itemImages[index]
+                            
+                            NavigationLink(destination: Text("Details for \(item)")) {
+                                VStack {
+                                    Image(imageName) // Use the image name here
+                                        .resizable()
+                                        .frame(width: 120, height: 120)
+                                        .foregroundColor(Color.green)
+                                    
+                                    Text(item)
+                                        .foregroundColor(Color.white)
+                                        .font(.caption)
+                                }
                             }
                         }
                     }
+                    .padding()
                 }
+                .accentColor(Color.green)
+                .navigationTitle("Study Resources")
             }
-            .accentColor(Color.green)
         }
-        .navigationTitle("Study Resources")
+        
+        let items = ["Textbooks", "Novel", "Syllabus", "Universities", "Polytechnics", "UTME", "Direct Entry", "Important Dates", "Tips"]
     }
-     
-     let items = ["Textbooks", "Novel", "Syllabus", "Leadership board", "Universities", "Polytechnics", "UTME", "Direct Entry", "Important Dates", "Tips"]
- }
 
-struct StudyResources_Previews: PreviewProvider {
-    static var previews: some View {
-        StudyResources()
+    struct StudyResources_Previews: PreviewProvider {
+        static var previews: some View {
+            StudyResources()
+        }
     }
-}
